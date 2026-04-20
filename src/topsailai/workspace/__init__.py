f2ff7844c54b259989bb97f5df9b43e36e89c494
DawsonLin <lin_dongsen@126.com>
2026-04-20 19:54:33 +0800

    AI:  Initialize workspace module with automatic task folder creation

---
+++ src/topsailai/workspace/__init__.py
@@ -0,0 +1,12 @@
+'''
+Author: DawsonLin
+Email: lin_dongsen@126.com
+Created: 2026-04-20
+Purpose:
+'''
+
+import os
+
+from .folder_constants import FOLDER_WORKSPACE_TASK
+
+os.makedirs(FOLDER_WORKSPACE_TASK, exist_ok=True)
