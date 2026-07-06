067279d9067948c6c7729a247eddfbf85158ad01
DawsonLin <lin_dongsen@126.com>
2026-07-06 18:27:42 +0800

    Style: clarify interactive mode log message with env var name

--- src/topsailai/ai_base/tool_call.py
+++ src/topsailai/ai_base/tool_call.py
@@ -63,9 +63,9 @@ class StepCallBase(object):
 
         if env_tool.EnvReaderInstance.get("TOPSAILAI_CHAT_INTERACTIVE_MODE") is not None:
             self.flag_interactive = env_tool.EnvReaderInstance.check_bool("TOPSAILAI_CHAT_INTERACTIVE_MODE")
-        logger.info("interactive mode: [%s]", self.flag_interactive)
+        logger.info("chat interactive mode (TOPSAILAI_CHAT_INTERACTIVE_MODE): [%s]", self.flag_interactive)
 
-        # print_step(f"interactive mode: [{self.flag_interactive}]", need_format=False)
+        # print_step(f"chat interactive mode (TOPSAILAI_CHAT_INTERACTIVE_MODE): [{self.flag_interactive}]", need_format=False)
 
         # internal variables, self._xxx
         # last step name, string
