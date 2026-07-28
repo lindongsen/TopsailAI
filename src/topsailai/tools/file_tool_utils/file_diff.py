667f7b6dc43bac1832a570bb68a7524a5453664e
DawsonLin <lin_dongsen@126.com>
2026-07-28 13:21:26 +0800

    chore: file_diff update tips

--- src/topsailai/tools/file_tool_utils/file_diff.py
+++ src/topsailai/tools/file_tool_utils/file_diff.py
@@ -129,7 +129,7 @@ def compare_files_strived(file1_path, file2_path, context_lines=3) -> str:
     except Exception:
         pass
 
-    return "---\nNothing Changed"
+    return "\n---\nNothing Changed"
 
 
 # Usage example
